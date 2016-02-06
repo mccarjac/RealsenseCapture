e5b4ed3af9b6c50dc2af14b44965b989c77e4b57
Jacob McCarthy <mccarjac@gmail.com>
2016-02-06 12:57:23 -0800

    Dividing code into functions

--- Source.cpp
+++ Source.cpp
@@ -8,6 +8,13 @@
 
 using namespace cv;
 
+struct dubCam
+{
+	PXCCapture::DeviceInfo one;
+	PXCCapture::DeviceInfo two;
+};
+
+dubCam getDeviceInfo(PXCSenseManager * mySenseMan);
 void CaptureDeviceStream(PXCSenseManager * sm, PXCSenseManager * sm2, PXCCapture::DeviceInfo dev1, PXCCapture::DeviceInfo dev2, pxcCHAR * file, pxcCHAR * file2);
 UtilRender *renderColor = new UtilRender(L"COLOR STREAM");
 UtilRender *renderColor2 = new UtilRender(L"COLOR STREAM");
@@ -16,54 +23,21 @@ void main()
 {
 
 char c;
-PXCCapture::DeviceInfo camOne, camTwo;
 
 //Create our SDK Session for Module Management
 PXCSenseManager *sm = PXCSenseManager::CreateInstance();
 PXCSenseManager *sm2 = PXCSenseManager::CreateInstance();
 
-//Session Module - Create and Query instances of I/O and algorithm implmentations
-PXCSession::ImplVersion v = sm->QuerySession()->QueryVersion();
-
-//Create an ImplDesc to capture our video module implementation
-PXCSession::ImplDesc desc1 = {};
-desc1.group = PXCSession::IMPL_GROUP_SENSOR;
-desc1.subgroup = PXCSession::IMPL_SUBGROUP_VIDEO_CAPTURE;
-
-for (int m = 0;; m++)
-	{
-	PXCSession::ImplDesc desc2 = {};
-	if (sm->QuerySession()->QueryImpl(&desc1, m, &desc2) < PXC_STATUS_NO_ERROR) break;
-	wprintf_s(L"Module[%d]: %s\n", m, desc2.friendlyName);
-
-	PXCCapture *captureOne = 0;
-
-	pxcStatus sts = sm->QuerySession()->CreateImpl<PXCCapture>(&desc2, &captureOne);
-	//Create CaptureManager Module.
-	if (sts < PXC_STATUS_NO_ERROR) continue;
-	PXCCapture::DeviceInfo dinfo;
-
-	for (int d = 0;; d++)
-	{
-		sts = captureOne->QueryDeviceInfo(d, &dinfo);
-		if (d == 2) camOne = dinfo;
-		
-		else if (d == 3) camTwo = dinfo;
-		
-		if (sts < PXC_STATUS_NO_ERROR) break;
-		wprintf_s(L"	Device[%d]: %s\n", d, dinfo.name);
-	}
-	captureOne->Release();
-}
+struct dubCam currentDevices;
+//Lets get our current devices that are plugged in
+currentDevices = getDeviceInfo(sm);
 
 pxcCHAR * firstCam = L"C:/Users/Group34/Desktop/cam1.rssdk";
 pxcCHAR * secCam = L"C:/Users/Group34/Desktop/cam2.rssdk";
 
+//For now we just capture the RGB streams from each camera but can easily add functionalities to mess with depth and IR
+CaptureDeviceStream(sm,sm2, currentDevices.one,currentDevices.two, firstCam,secCam);
 
-//Lets try to kick off two threads that simaeltaneously grab the camera streams..
-CaptureDeviceStream(sm,sm2, camOne,camTwo, firstCam,secCam);
-
-wprintf_s(L"After\n");
 //Keep our session alive as long as we are accessing modules within it
 sm->Release();
 sm2->Release();
@@ -71,9 +45,46 @@ printf_s("Just making sure the programs working\n");
 scanf_s("%c", &c);
 }
 
-PXCCapture::DeviceInfo getDeviceInfo(PXCSenseManager * mySenseMan)
+dubCam getDeviceInfo(PXCSenseManager * mySenseMan)
 {
+	//Session Module - Create and Query instances of I/O and algorithm implmentations
+	PXCCapture::DeviceInfo camOne, camTwo;
+	struct dubCam myCameras = {};
+	PXCSession::ImplVersion v = mySenseMan->QuerySession()->QueryVersion();
 
+	//Create an ImplDesc to capture our video module implementation
+	PXCSession::ImplDesc desc1 = {};
+	desc1.group = PXCSession::IMPL_GROUP_SENSOR;
+	desc1.subgroup = PXCSession::IMPL_SUBGROUP_VIDEO_CAPTURE;
+
+	for (int m = 0;; m++)
+	{
+		PXCSession::ImplDesc desc2 = {};
+		if (mySenseMan->QuerySession()->QueryImpl(&desc1, m, &desc2) < PXC_STATUS_NO_ERROR) break;
+		wprintf_s(L"Module[%d]: %s\n", m, desc2.friendlyName);
+
+		PXCCapture *captureOne = 0;
+
+		pxcStatus sts = mySenseMan->QuerySession()->CreateImpl<PXCCapture>(&desc2, &captureOne);
+		//Create CaptureManager Module.
+		if (sts < PXC_STATUS_NO_ERROR) continue;
+		PXCCapture::DeviceInfo dinfo;
+
+		for (int d = 0;; d++)
+		{
+			sts = captureOne->QueryDeviceInfo(d, &dinfo);
+			if (d == 2) camOne = dinfo;
+
+			else if (d == 3) camTwo = dinfo;
+
+			if (sts < PXC_STATUS_NO_ERROR) break;
+			wprintf_s(L"	Device[%d]: %s\n", d, dinfo.name);
+		}
+		captureOne->Release();
+	}
+	myCameras.one = camOne;
+	myCameras.two = camTwo;
+	return myCameras;//camOne, camTwo;
 
 }
 
